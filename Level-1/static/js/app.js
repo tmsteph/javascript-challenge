98fd0b3a06cf944c19df053df98767452c58ea97
tmsteph <tmsteph1290@gmail.com>
2021-05-15 16:48:50 -0700

    First draft app.py

--- Level-1/static/js/app.js
+++ Level-1/static/js/app.js
@@ -1,9 +1,51 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+//Get a reference to the table Body
+var tbody = d3.select("tbody");
 
+// UFO sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    // Append one table row 'tr' for each UFO Sighting object
+    var row = tbody.append("tr");
+    
+    // Use 'Object.entries' to console.log each UFO sighting value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
 
-//for dictionary in tableData
-//
-// 
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tbody.html("")
+
+    // Select the input date get the raw HTML nodes
+    var inputElement = d3.select("#datetime");
+    // Get the value property of the input date, state, shape
+    var inputValue = inputElement.property("value");
+    //console.log input value
+    console.log(inputValue);
+    // Filter Data with datetime equal to input value
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // console.log filter values
+    console.log(filteredData);
+
+    filteredData.forEach(function(selections) {
+
+        console.log(selections);
+        // Append one table row "tr" for each UFO Sighting object
+        var row = tbody.append("tr");
+        // Use "Object.entries" to ocnsole.log each UFO Sighting value
+        Object.entries(selections).forEach(function([key, value]) {
+            console.log(key, value);
+            //append a cell to the row for each value
+            var cell = row.append("td");
+            cell.text(value)
+    });
+});
